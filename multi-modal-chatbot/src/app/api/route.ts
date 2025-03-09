63e2f9b8c7a95a78baae9b7c5d849876d8633df2
髙草木 和史 <kazushi.takakusagi@gmail.com>
2025-03-09 19:52:05 +0900

    コメントを残す

--- multi-modal-chatbot/src/app/api/route.ts
+++ multi-modal-chatbot/src/app/api/route.ts
@@ -4,13 +4,20 @@ import { streamText, Message } from 'ai';
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
+/* 
+* messages: 会話の履歴
+* 会話作成のapi endpointをapp/api/postに設置.
+*/
+
 export async function POST(req: Request) {
   const { messages }: { messages: Message[] } = await req.json();
 
+  // モデルを指定し、会話を作成.
   const result = streamText({
     model: openai('gpt-4o'),
     messages,
   });
 
+  // ストリームを返す.
   return result.toDataStreamResponse();
 }
